a9093e5a2b9dbcf6001c0b266c876f6ee02029cc
Carolina Uvieta <c.b.uvieta@gmail.com>
2026-07-05 21:24:44 -0300

    Mejoras en el hero y slider

--- js/script.js
+++ js/script.js
@@ -4,3 +4,41 @@ const mobileMenu = document.querySelector(".nav-mobile");
 toggle.addEventListener("click", () => {
   mobileMenu.classList.toggle("active");
 });
+
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+
+let current = 0;
+
+function showSlide(index){
+
+    slides.forEach(slide => slide.classList.remove("active"));
+    dots.forEach(dot => dot.classList.remove("active"));
+
+    slides[index].classList.add("active");
+    dots[index].classList.add("active");
+
+    current = index;
+}
+
+setInterval(() => {
+
+    current++;
+
+    if(current >= slides.length){
+        current = 0;
+    }
+
+    showSlide(current);
+
+},5000);
+
+dots.forEach((dot,index)=>{
+
+    dot.addEventListener("click",()=>{
+
+        showSlide(index);
+
+    });
+
+});
